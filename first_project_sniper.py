7b6fad0c12e72052c79567fe0523646774ba3fad
ehomak269-crypto <ehomak269@gmail.com>
2026-04-03 18:39:50 +0500

    2 commit

--- first_project_sniper.py
+++ first_project_sniper.py
@@ -4,8 +4,12 @@ from random import randint
 window = display.set_mode((700, 500))
 background = transform.scale(image.load('backg_sniper2.png'), (700, 500))
 scope_frame = image.load('scope_3.png')
-
 scope_frame.set_colorkey((255, 255, 255))
+
+mixer.init()
+shoot = mixer.Sound('shoot2.mp3')
+reload_sound = mixer.Sound('reload.mp3')
+shoot.set_volume(0.1)
 
 clock = time.Clock()
 run = True
@@ -32,27 +36,78 @@ class Player(Gamesprite):
             self.rect.y += 5
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= 5
-    #def fire(self):
-    #    bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 8, 15, 20)
-    #    bullets.add(bullet)
-    
+
+        
+
+
+class Enemy(Gamesprite):
+    def __init__(self, player_image, x, y, speed, width = 50, height = 50):
+        super().__init__(player_image, x, y, speed, width, height)
+        self.start = x
+        self.direction = 'right'
+        
+    def update(self):
+        if self.rect.x <= self.start - 50:
+            self.direction = 'right'
+        if self.rect.x > self.start + 50:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+
+        
+
+enemys = sprite.Group()
+for i in range(2):
+    enemy = Enemy('enemy3.png', randint(150, 550), randint(150, 350), randint(1, 3), 20, 30)
+    enemys.add(enemy)
 
 scope = Player('scope.png', 0, 0, 3, 200, 200)
 mask = Surface((700, 500), SRCALPHA)
+
+ammo = 10
+reload_time = 2000
+reload = False
+reload_start = 0
+font.init()
+
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-        #if e.type == KEYDOWN:
-        #    if e.key == K_SPACE:
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE and reload == False:
+                if ammo > 0:
+                    shoot.play()
+                    ammo -= 1
+                    for enemy in enemys:
+                        if enemy.rect.collidepoint(scope.rect.center):
+                            enemys.remove(enemy)
+                            enemy = Enemy('enemy3.png', randint(150, 550), randint(150, 350), randint(1, 3), 20, 30)
+                            enemys.add(enemy)
+                if ammo == 0:
+                    reload = True
+                    reload_sound.play()
+                    reload_start = time.get_ticks()
+    if reload == True:
+        now = time.get_ticks()
+        if now - reload_start >= reload_time:
+            ammo = 10
+            reload = False 
+
     window.blit(background, (0, 0))
     scope.move()
     scope.reset()
+    enemys.draw(window)
+    enemys.update()
     mask.fill((0, 0, 0, 255))
     draw.circle(mask, (0, 0, 0, 0), scope.rect.center, 95)
     window.blit(mask, (0, 0))
-    
+    ammo_text = font.SysFont('Arial', 30).render('Ammo: ' + str(ammo) + '/10', True, (255, 255, 255))
+    window.blit(ammo_text, (525, 450))
     display.flip()
     clock.tick(60)
             
